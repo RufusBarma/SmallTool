3d8e38731cd23833a27c33fcf782a07d80ce8144
RufusBarma <neymanyu@gmail.com>
2021-11-19 16:58:09 +0500

    Add status in view

--- SmallTool/Models/BranchTuple.cs
+++ SmallTool/Models/BranchTuple.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using LanguageExt;
 using LanguageExt.SomeHelp;
+using Microsoft.AspNetCore.Html;
 
 namespace SmallTool.Models
 {
@@ -18,10 +19,40 @@ namespace SmallTool.Models
     
     public static class BranchTupleExtension
     {
-        public static BranchStatus GetStatus(this BranchTuple tuple)
+        public static HtmlString GetColorString(this BranchStatus status)
         {
-            // if (tuple.LocalBuild.IsNone && tuple.RemoteBuild.IsNone)
-                return BranchStatus.Manual;
+            throw new NotImplementedException();
         }
+        
+        public static string GetString(this BranchStatus status) => status switch
+            {
+                BranchStatus.Manual => "ручаня сборка",
+                BranchStatus.Actual => "последняя версия",
+                BranchStatus.Outdated => "устаревшая версия",
+                BranchStatus.Deleted => "удалена на сервере",
+                BranchStatus.New => "есть обновления",
+                _ => throw new ArgumentOutOfRangeException(nameof(status), status, null)
+            };
+        
+        public static Build GetFresh(this Branch branch) => branch.Builds.OrderBy(b => b.CreatedTime).First();
+        public static BranchStatus GetStatus(this BranchTuple tuple) =>
+            tuple.LocalBuild.Match(
+                local => tuple.RemoteBuild.Match(remote =>
+                    {
+                        var localBuild = local.GetFresh();
+                        var remoteBuild= remote.GetFresh();
+                        return localBuild.CreatedTime < remoteBuild.CreatedTime 
+                            ? BranchStatus.Outdated 
+                            : BranchStatus.Actual;
+                    },
+                    BranchStatus.Deleted),
+                ()=> tuple.RemoteBuild.Match(_ => BranchStatus.New, BranchStatus.Deleted));
+
+        public static Option<string> GetName(this BranchTuple tuple) => 
+            tuple.LocalBuild.Match(
+                branch => Option<string>.Some(branch.Name), 
+                tuple.RemoteBuild.Match(
+                        rb => Option<string>.Some(rb.Name), 
+                        Option<string>.None));
     }
 }
