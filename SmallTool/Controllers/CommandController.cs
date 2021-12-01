4b71dff97bf011878a0f822b65a8ba6ac4cba333
RufusBarma <neymanyu@gmail.com>
2021-12-01 17:11:15 +0500

    Add command controller

---
+++ SmallTool/Controllers/CommandController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmallTool.Controllers
+{
+    public class CommandController : Controller
+    {
+        [HttpPost]
+        public IActionResult RunCommand(string command, string branch)
+        {
+            return Ok();
+        }
+    }
+}
